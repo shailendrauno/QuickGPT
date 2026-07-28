d31b87a163a7aedb45716eca77f43867f3073a0f
Shailendra Kumar <shailendrauno89@gmail.com>
2026-07-28 21:09:48 +0530

    first commit

---
+++ server/models/User.js
@@ -0,0 +1,39 @@
+import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    credits: {
+        type: Number,
+        default: 20,
+    },
+});
+
+
+// Hash password before saving
+userSchema.pre('save', async function () {
+    // If password isn't modified, just exit the function
+    if (!this.isModified('password')) {
+        return;
+    }
+    
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+});
+
+const User = mongoose.models.User || mongoose.model("User", userSchema);
+
+
+export default User;
---
+++ server/server.js
@@ -0,0 +1,39 @@
+import express from 'express'
+import 'dotenv/config'
+import cors from 'cors'
+import connectDB from './configs/db.js'
+import userRouter from './routes/userRoutes.js'
+import chatRouter from './routes/chatRoutes.js'
+import messageRouter from './routes/messageRoutes.js'
+import creditRouter from './routes/creditRoutes.js'
+import { stripeWebhooks } from './controllers/webhooks.js'
+
+const app = express()
+
+await connectDB();
+
+// Stripe webhook
+
+app.post('/api/stripe', express.raw({type: 'application/json'}), stripeWebhooks)
+
+///Middleware
+
+app.use(cors())
+app.use(express.json())
+
+//Route
+
+app.get('/', (req,res) => {
+    res.send('Server is live')
+})
+
+app.use('/api/user', userRouter)
+app.use('/api/chat', chatRouter)
+app.use('/api/message', messageRouter)
+app.use('/api/credit', creditRouter)
+
+const PORT = process.env.PORT || 3000
+app.listen(PORT, ()=> {
+    console.log(`Server is runnding on port ${PORT }`);
+    
+})
